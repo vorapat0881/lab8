d802deff7521cb3e802b15003727a68d6d450662
vorapat0881 <vorapat_g@cmu.ac.th>
2023-12-12 16:36:25 +0700

    lab8_3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age, h, b;
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age <= 25){
+        cout << "Enter your height: ";
+        cin >> h;
+        if (h < 100){
+        cout << "Your character = Chopper";
+        }
+        else if (h < 180){
+        cout << "Your character = Usopp";
+        }
+        else {cout << "Enter your bounty: ";
+            
+            cin >> b;
+            if (b > 1100000000){
+                cout << "Your character = Zoro";
+            }
+            else cout << "Your character = Sanji";}
+    }
+    else if (age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> b;
+        if (b > 500000000){
+            cout << "Your character = Jinbe";
+        }
+        else cout << "Your character = Franky";
+    }
+    else cout << "Your character = Brook";
+
+    return 0;
+}
+
 /*
 "Enter your age: "
 "Enter your height: "
